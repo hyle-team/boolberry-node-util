577af1adcff20bf459ac00345600b2297d374644
cr.zoidberg <crypto.zoidberg@gmail.com>
2019-03-14 21:14:22 +0100

    attempt to fix sigfault

--- src/main.cc
+++ src/main.cc
@@ -149,7 +149,7 @@ void get_pow_hash(const Nan::FunctionCallbackInfo<v8::Value>& args) {
     //boolberry_hash(input, input_len, scratchpad, spad_len, output, height);
     crypto::wild_keccak_dbl_opt(reinterpret_cast<const uint8_t*>(input), input_len,
             reinterpret_cast<uint8_t *>(&output[0]), sizeof(output), 
-            (const UINT64*)&scratchpad[0], spad_len/4);
+            (const UINT64*)&scratchpad[0], spad_len/8);
 
     v8::Isolate* isolate = args.GetIsolate();
 
